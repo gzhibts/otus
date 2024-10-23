0ffcf586f60f83e5bbf4fe8f6ca5beb4f12d0ec5
gzhibts <gzhibtsova@gmail.com>
2024-10-23 23:32:20 +0300

    v 1

---
+++ L21-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,46 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "phone")
+@Data
+public class Phone implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "phonenumber")
+    private String number;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Client client;
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public Phone(String number) {
+        this.id = null;
+        this.number = number;
+    }
+
+    public Phone() {
+    }
+
+
+    @Override
+    public Phone clone() {
+        try {
+            Phone phone = (Phone) super.clone();
+            phone.setId(this.id);
+            phone.setNumber(this.number);
+            return phone;
+        } catch (CloneNotSupportedException e) {
+            throw new UnsupportedOperationException(e);
+        }
+
+    }
+}
