77b3c6b878c2efdd8f20f268ee3b41820c7163d9
gzhibts <gzhibtsova@gmail.com>
2024-12-19 13:58:01 +0300

    v 1

---
+++ L24-webServer/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,89 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @SequenceGenerator(name = "client_gen", sequenceName = "client_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, targetEntity = Phone.class, fetch = FetchType.EAGER, mappedBy = "client")
+    private List<Phone> phones = new ArrayList<>();
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @SuppressWarnings("this-escape")
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = phones;
+        for (Phone phone : phones) {
+            phone.setClient(this);
+        }
+    }
+
+    @Override
+    @SuppressWarnings({"java:S2975", "java:S1182"})
+    public Client clone() {
+
+        var clone = new Client(this.id, this.name);
+
+        if (this.address != null) {
+            clone.setAddress(this.address.clone());
+        }
+
+        if (this.phones != null) {
+            List<Phone> clonedPhones = new ArrayList<>();
+
+            for (var phone : this.phones) {
+                Phone cloned = phone.clone();
+                cloned.setClient(clone);
+                clonedPhones.add(cloned);
+            }
+
+            clone.setPhones(clonedPhones);
+        }
+
+        return clone;
+    }
+
+    @Override
+    public String toString() {
+        String addressString = "";
+        if (address != null) {
+            addressString = "street=" + address.getStreet();
+        }
+        return "Client{" + "id=" + id + ", name='" + name + "', address='" + addressString + '}';
+    }
+}
---
+++ L24-webServer/src/main/java/ru/otus/crm/service/DBServiceClient.java
@@ -0,0 +1,19 @@
+package ru.otus.crm.service;
+
+import java.util.List;
+import java.util.Optional;
+import ru.otus.crm.model.Client;
+
+public interface DBServiceClient {
+
+    Client saveClient(Client client);
+
+    Optional<Client> getClient(long id);
+
+    List<Client> findAll();
+
+    Optional<Client> findById(long id);
+
+    Optional<Client> findRandomClient();
+
+}
